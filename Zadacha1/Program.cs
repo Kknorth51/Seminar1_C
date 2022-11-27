1b62e8f6c1f09ffbe6431be07d4d70640a51912a
kknorth <kirillacid@gmail.com>
2022-11-27 18:16:26 +0700

    Цикл

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -3,3 +3,18 @@ int num = Convert.ToInt32(Console.ReadLine());
 int square = num*num;
 //Console.WriteLine("Квадрат введённого числа равен "+ square);
 Console.WriteLine($"Квадрат введённого числа равен {square}");
+
+if (num > 9)
+{
+    Console.WriteLine("Вы ввели число больше девяти");
+}
+else
+{
+    Console.WriteLine("Вы ввели число меньше десяти"); 
+}
+
+while (num<=25) // Цикл выводит все числа от введенные до 25
+{
+    Console.WriteLine($" {num} ");
+    num++;
+}
