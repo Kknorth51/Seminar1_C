607364151d0283857fee36e281895e46cc8b1945
kknorth <kirillacid@gmail.com>
2022-11-27 18:02:35 +0700

    Пример форматированного вывода на экран

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.WriteLine("Введите целое число ");
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num*num;
-Console.WriteLine("Квадрат введённого числа равен "+ square);
+//Console.WriteLine("Квадрат введённого числа равен "+ square);
+Console.WriteLine($"Квадрат введённого числа равен {square}");
